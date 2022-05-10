b1310c5a6817e3da4af9478c84622f0a3985450f
Christina <christinaa126@gmail.com>
2022-05-10 09:37:32 -0700

    added one to many association

--- models/index.js
+++ models/index.js
@@ -1,5 +1,14 @@
 const User = require("./User");
+const Post = require("./Post");
 
 // Define sequelize associations in this file.
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+  });
+  
+  Post.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
 
-module.exports = { User };
+module.exports = { User, Post };
