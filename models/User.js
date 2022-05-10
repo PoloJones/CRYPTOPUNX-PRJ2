e0d537120fb1a8e9f4ae1c515b5f51fcb197845a
Christina <christinaa126@gmail.com>
2022-05-10 10:52:43 -0700

    added userdata and postdata seeds

--- models/User.js
+++ models/User.js
@@ -22,19 +22,19 @@ User.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [8],
-      },
-    },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
       validate: {
         isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
       },
     },
   },
