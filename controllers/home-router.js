f5d4bd7ee0e98dcd28e1e12c372255016814c720
Christina <christinaa126@gmail.com>
2022-05-11 13:24:44 -0700

    add postdata to seeds/index.js to populate data

--- controllers/home-router.js
+++ controllers/home-router.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 const router = require("express").Router();
-const { User } = require("../models");
+const { User, Post } = require("../models");
 const apikey = "8bd75f8c-d432-4f8c-83de-df36a896d752";
 // use withAuth middleware to redirect from protected routes.
 // const withAuth = require("../util/withAuth");
@@ -14,8 +14,19 @@ router.get("/", async (req, res) => {
   try {
     const response = await fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY='+ apikey + "&start=1&limit=5&convert=USD");
     const data = await response.json();
+    //code to display all posts
+    const postData = await Post.findAll({
+      include: [
+          {
+            model: User,
+            attributes: ['username'],
+          },
+        ],
+  });
+  // Serialize data so the template can read it
+  const posts = postData.map((post) => post.get({ plain: true }));
+ 
 
-console.log(data);
     let user;
     if (req.session.isLoggedIn) {
       user = await User.findByPk(req.session.userId, {
@@ -23,11 +34,12 @@ console.log(data);
         raw: true,
       });
     }
-    res.render("home", {
+    res.render("homepage", {
       title: "Home Page",
       isLoggedIn: req.session.isLoggedIn,
       user,
       data,
+      posts,
     });
   } catch (error) {
     console.error(error);
