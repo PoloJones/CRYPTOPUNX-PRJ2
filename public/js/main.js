6c5f0b71811592eea82b5b2f60700099e432d56b
Isaac Jones <isaaclawsonjones@gmail.com>
2022-05-12 20:59:33 -0700

    pls

--- public/js/main.js
+++ public/js/main.js
@@ -7,3 +7,28 @@ document.querySelector(".btn-logout").addEventListener("click", async () => {
     console.error("Failed to logout.");
   }
 });
+
+var canvasElement = document.getElementById("coinChart");
+var config = {
+    type: "bar",
+    data: { 
+        labels: ["BitCoin", "Ethereum", "DogeCoin"],
+        datasets: [
+            {
+                label: "Price(USD)",
+                data: [5, 10, 15, 100],
+                backgroundColor: [
+                    "rgba(255, 0, 247, 1)", //Pink
+                    "rgba(0, 255, 13, 1)", //green
+                    "rgba(0, 19, 255, 1)", //Blue
+                ],
+                borderColor: [],
+                dataColor: [
+                    "rgba(255, 255, 255, 1)"
+                ],
+            },
+        ],
+    },
+};
+
+var coinChart = new Chart (canvasElement, config);
